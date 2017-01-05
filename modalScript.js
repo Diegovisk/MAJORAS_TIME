76abd7fbdb74f3626816e2a5da1d6feac0556be0
STANLEY\Stanley <rick-stanley@hotmail.com>
2017-01-05 12:21:10 -0400

    shortcut added

--- modalScript.js
+++ modalScript.js
@@ -2,15 +2,15 @@
 var modal = document.getElementById('menu');
 
 // Get the button that opens the modal
-var btn = document.getElementById("openModal");
+// var btn = document.getElementById("openModal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function(){
-    modal.style.display = "block";
-}
+// btn.onclick = function(){
+//     modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function(){
@@ -23,3 +23,12 @@ window.onclick = function(event){
         modal.style.display = "none";
     }
 }
+
+    // This only work on generic based keyboards, and of course most of them are directed to windows users
+    // we have to keep mac in mind
+// document.onkeyup=function(e){
+//   if(e.altKey && e.which == 79) {
+//     // alert('Keyboard shortcut working!');
+//     modal.style.display = "block";
+//   }
+// }
