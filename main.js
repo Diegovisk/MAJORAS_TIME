15877d428d8e933cd4f03c3c87c151bb3797bcce
STANLEY\Stanley <rick-stanley@hotmail.com>
2017-01-12 18:41:34 -0400

    script for packaging

--- main.js
+++ main.js
@@ -8,19 +8,27 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 // We need to store the "id" of the current window to destroy it later on
+// the array always starts with 0, keep that in mind
 var inSession=[],index=0;
 let tray = null
 function createWindow () {
   var isFullscreen = true;
   // Create the browser window.
-  win = new BrowserWindow({backgroundColor: '#000000',icon:'./res/mipmap-hdpi/ic_launcher.png',width: 1280,height: 1000})
-  win.setFullScreen(true);
+  win = new BrowserWindow({backgroundColor: '#000000',icon:'./res/mipmap-xxxhdpi/ic_launcher.png',width: 1280,height: 1000})
+  
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
+
+  // We need this to bypass the taskbar error
+  // watch those 10 milliseconds, they're precious
+  setTimeout(function(){
+    inSession[index-1].setFullScreen(true);
+  },10)
+
   inSession[index] = win;
   index++;
   // Open the DevTools.
@@ -58,6 +66,10 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+
+// "Why not use win.show(); or win.restore(); electron methods to show/restore the window?"
+// R: To restart the transition/animation.
+// "But you can use JavaScript or JQUERY to do this". R => yeah...
 app.on('ready', () => {
   tray = new Tray('./res/mipmap-hdpi/ic_launcher.png')
   const contextMenu = Menu.buildFromTemplate([
@@ -67,13 +79,12 @@ app.on('ready', () => {
       //we have to jump back two array units to destroy the previous window, after the new one has been created
       // if we don't do this, the current session/window will be destroyed as soon as created
       inSession[index-2].destroy();
-      // win.restore();
     }},
     {label: 'Quit', click: function() {
       app.quit()
     }}
   ])
-  tray.setToolTip("Majora's Time")
+  tray.setToolTip('Majora\'s Time')
   //This is for Linux
   contextMenu.items[1].checked = false
   tray.setContextMenu(contextMenu)
