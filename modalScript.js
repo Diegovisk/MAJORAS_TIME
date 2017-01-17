6fa9e93e0db41a096257c9ccf518957f9008a6c1
STANLEY\Stanley <rick-stanley@hotmail.com>
2017-01-17 14:45:32 -0400

    draggable body fix/user related, Arial workaround

--- modalScript.js
+++ modalScript.js
@@ -24,15 +24,15 @@ if (body !== null) {
 
 // Get the modal and your content to modify/execute the animation, these guys
 // they have to come after the "body test"
-modal = document.getElementById('menu');
-content = document.getElementById('content');
+modal = document.getElementById('menu').style;
+content = document.getElementById('content').style;
 
 // Get the <span> element that closes the modal
 span = document.getElementsByClassName("close")[0];
 
 if (isIndex) {
-    content.style.animation = "animatetop 0.4s";
-    modal.style.display = "none";
+    content.animation = "animatetop 0.4s";
+    modal.display = "none";
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
@@ -50,22 +50,35 @@ if (isIndex) {
     // directed to windows users we have to keep mac in mind
     document.onkeyup = function (event) {
         var x = event.which || event.keyCode;
-        if ((x === 79) && (modal.style.display !== "block")) {
-            body.style.cursor = "default";
-            modal.style.display = "block";
-        } else if ((x === 88 || x === 27 || x === 79) && (modal.style.display === "block")) {
+        if ((x === 79) && (modal.display !== "block")) {
+            body.cursor = "default";
+            modal.display = "block";
+        } else if ((x === 88 || x === 27 || x === 79) && (modal.display === "block")) {
             hide();
         }
     }
 
     //this  reverts the animatetop, and put it back in place ready for action again
     function hide() {
-        content.style.animation = "animatetopRe 0.4s";
+        content.animation = "animatetopRe 0.4s";
         setTimeout(function () {
-            content.style.animation = "animatetop 0.4s";
-            modal.style.display = "none";
+            content.animation = "animatetop 0.4s";
+            modal.display = "none";
         }, 400)
     }
 } else {
-    modal.style.display = "block";
+    // We must access the BrowserWindow object created by our main process and call the minimize, 
+    // maximize, and close methods on that. We can access this using the 'remote' module
+    var remote = require('electron').remote, win;
+    // body = document.getElementById('modalBody');
+    
+    modal.display = "block";
+    modal.paddingTop = "0px";
+    content.height = "auto";
+    content.width = "auto";
+    
+    span.onclick = function () {
+        win = remote.getCurrentWindow();
+        win.close();
+    }
 }
